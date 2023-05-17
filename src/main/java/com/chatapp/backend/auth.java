34e1d76a80c23b7b83e6be080b99ba5e7ffd0891
henry <henryboy753951@gmail.com>
2023-05-17 16:34:13 +0800

    gogo

--- src/main/java/com/chatapp/backend/auth.java
+++ src/main/java/com/chatapp/backend/auth.java
@@ -3,27 +3,40 @@ package com.chatapp.backend;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.chatapp.backend.entity.BaseResponse;
+import com.chatapp.backend.entity.User;
 import com.chatapp.backend.model.UserRepository;
 import com.chatapp.backend.model.user;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import com.chatapp.backend.utils;
+
 
 @RestController
 @RequestMapping("/auth")
 public class auth {
     @RequestMapping(value= "/register",method = RequestMethod.PUT)
-    public BaseResponse<String> register(
+    public BaseResponse<User> register(
             @RequestParam(value = "username", required = false) String username,
             @RequestParam(value = "password", required = false) String password) {
-        BaseResponse<String> response = new BaseResponse<String>("成功!");
-        // ...);
+        BaseResponse<User> response = new BaseResponse<User>("成功!");
+        // ...
+        
+        User user = new User();
+        user.username = username;
+        user.Name = "test";
+        user.email = username+"@mail.nuk.edu.tw";
+        user.department = "test department";
+        user.token = utils.sha256(username);
+        
+        
 
-        response.data = username;
+        response.data = user;
         return response;
     }
 }
---
+++ src/main/java/com/chatapp/backend/entity/User.java
@@ -0,0 +1,12 @@
+package com.chatapp.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL) // 若參數是 null 則不顯示
+public class User{
+    public String username;
+    public String Name;
+    public String email;
+    public String token;
+    public String department;
+}
