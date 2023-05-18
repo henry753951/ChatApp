331a793cc85205e394e18ba5ad3089556559a5d8
henry <henryboy753951@gmail.com>
2023-05-19 06:05:26 +0800

    login done

--- src/main/java/com/chatapp/backend/controllers/auth.java
+++ src/main/java/com/chatapp/backend/controllers/auth.java
@@ -1,9 +1,13 @@
 package com.chatapp.backend.controllers;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.IOException;
+import java.net.http.HttpClient;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
+import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +20,7 @@ import com.chatapp.backend.model.userDB;
 import com.chatapp.backend.repository.UserRepository;
 import com.chatapp.backend.security.JwtUtil;
 import com.chatapp.backend.utils;
+import java.lang.reflect.Type;
 
 class UserLoginModel {
     public String username;
@@ -31,11 +36,26 @@ public class auth {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public BaseResponse<HashMap<String, Object>> login(HttpServletRequest request,
-            @RequestBody UserLoginModel userLoginModel) {
+            @RequestBody UserLoginModel userLoginModel) throws IOException {
         BaseResponse<HashMap<String, Object>> response = new BaseResponse<HashMap<String, Object>>();
         // ...
         userLoginModel.username = userLoginModel.username.toLowerCase();
-        if (false) {
+        OkHttpClient client = new OkHttpClient();
+        okhttp3.RequestBody body = new FormBody.Builder()
+        .add("account", userLoginModel.username)
+        .add("password", userLoginModel.password)
+        .build();
+        okhttp3.Request r = new Request.Builder()
+        .url("https://nukapp.tk/login?token=777app")
+        .post(body)
+        .build();
+        okhttp3.Response re = client.newCall(r).execute();
+        String json = re.body().string();
+        HashMap<String, Object> result = new Gson().fromJson(json, HashMap.class);
+        HashMap<String, Object> user_ = new Gson().fromJson(result.get("user").toString(), HashMap.class);
+        // print
+        System.out.println(result);
+        if (!result.containsKey("id")) {
             response.msg = "帳號或密碼錯誤";
             response.setError("授權失敗!");
             return response;
@@ -44,9 +64,9 @@ public class auth {
         response.msg = "成功登入";
         User user = new User();
         user.username = userLoginModel.username;
-        user.Name = "test";
+        user.Name = user_.get("student_name").toString();
         user.email = userLoginModel.username + "@mail.nuk.edu.tw";
-        user.department = "test department";
+        user.department = user_.get("student_department_name").toString();
         userDB userInDb = userRepository.findByUsername(userLoginModel.username);
         if (userInDb == null) {
             userInDb = new userDB();
@@ -55,7 +75,7 @@ public class auth {
             userRepository.save(userInDb);
         }
 
-        response.data = new HashMap<String,Object>();
+        response.data = new HashMap<String, Object>();
         response.data.put("token", JwtUtil.createToken(userInDb));
         response.data.put("user", user);
 
