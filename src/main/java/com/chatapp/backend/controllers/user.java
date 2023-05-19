28ee5bfcd27a5255de6c81a6e1e261324c96f2b6
binhan <qweqazhank123@gmail.com>
2023-05-19 21:57:36 +0800

    add friend

---
+++ src/main/java/com/chatapp/backend/controllers/user.java
@@ -0,0 +1,5 @@
+package com.chatapp.backend.controllers;
+
+public class user {
+    
+}
--- src/main/java/com/chatapp/backend/repository/UserRepository.java
+++ src/main/java/com/chatapp/backend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.chatapp.backend.repository;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.chatapp.backend.model.userDB;
 
 public interface UserRepository  extends MongoRepository<userDB, Object> {
   public userDB findByUsername(String username);
+  public userDB addFriend(String username, String friendName);
 }
