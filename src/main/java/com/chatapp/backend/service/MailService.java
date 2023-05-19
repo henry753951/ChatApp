3502607ababff7143eb5a8341c42fe9feff0602a
henry <henryboy753951@gmail.com>
2023-05-19 21:56:28 +0800

    fixed maybe

--- src/main/java/com/chatapp/backend/service/MailService.java
+++ src/main/java/com/chatapp/backend/service/MailService.java
@@ -1,14 +1,19 @@
 package com.chatapp.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.Async;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 @Service
+@EnableAsync
 public class MailService {
 
    @Autowired
     private JavaMailSender mailSender;
+
+    
 
     public void sendSimpleMail()  {
         SimpleMailMessage message = new SimpleMailMessage();
