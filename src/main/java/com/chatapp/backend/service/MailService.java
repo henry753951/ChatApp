bd1871c64331245aa9328062957ce9a89a2024cd
henry <henryboy753951@gmail.com>
2023-05-19 21:45:59 +0800

    some change

--- src/main/java/com/chatapp/backend/service/MailService.java
+++ src/main/java/com/chatapp/backend/service/MailService.java
@@ -3,17 +3,18 @@ package com.chatapp.backend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-
+import org.springframework.stereotype.Service;
+@Service
 public class MailService {
 
    @Autowired
     private JavaMailSender mailSender;
 
-    public void sendSimpleMail() throws Exception {
+    public void sendSimpleMail()  {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("jj.huang@hotmail.com");
-        message.setTo("jj.huang@*****.com.tw");
-        message.setSubject("主旨：Hello J.J.Huang");
+        message.setFrom("a1105534@mail.nuk.edu.tw");
+        message.setTo("a1105534@mail.nuk.edu.tw");
+        message.setSubject("測試測試");
         message.setText("內容：這是一封測試信件，恭喜您成功發送了唷");
 
         mailSender.send(message);
